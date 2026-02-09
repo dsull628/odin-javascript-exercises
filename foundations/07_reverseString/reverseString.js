2e5f2cd6c25c62c8a0c8aeb29db32a62658352e5
trafa <65374037+dsull628@users.noreply.github.com>
2026-02-08 16:32:33 -0800

    exercises 06-08

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversed = [];
 
+    for (let i = 0; i < string.length; i++) {
+        let stringIndex = string.length - 1 - i;
+        reversed[i] = string[stringIndex];
+    }
+
+    return reversed.join('');
 };
 
 // Do not edit below this line
--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (let i = 0; i < args.length; i++) {
+        const valueToRemove = args[i];
+        
+        for (let j = arr.length; j >= 0; j--) {
+            if (arr[j] === valueToRemove) {
+                arr.splice(j, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
